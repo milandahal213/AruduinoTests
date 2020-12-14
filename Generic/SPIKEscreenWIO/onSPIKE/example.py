a96df04f6a9d0b41e51adaf910b7d47806769df6
milandahal213 <milandahal213@gmail.com>
2020-12-14 11:34:03 -0500

    removed

--- Generic/SPIKEscreenWIO/onSPIKE/example.py
+++
@@ -1,5 +0,0 @@
-#example
-import serialWIO
-serialWIO.set_bg_color(99999) #still need to figure out the numbers and colors but it works
-serialWIO.set_font_size(2) #use 1 2 3 etc.
-serialWIO.text("MILAN",20,70) #20 and 70 are x and y
